19d78ac1bbc3f72f9f630f73e64cf2ab3995496d
Shourya <shourya.bafna2019@gmail.com>
2024-12-05 10:02:03 +0530

    Added Functionality in the skip button.

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-items");
+
 hamburger.addEventListener("click", function (e) {
   navMenu.classList.toggle("dis");
   hamburger.classList.toggle("active");
-  //   hamburger.innerHTML = <i class="fa-solid fa-xmark"></i>
+});
+
+const skip = document.querySelector(".skip");
+const getintouch = document.querySelector(".getintouch");
+
+skip.addEventListener("click", function (e) {
+  getintouch.classList.toggle("disable");
 });
