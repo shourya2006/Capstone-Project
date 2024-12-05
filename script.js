7ff938a66c8c1ad70b4844eff402cacddccc3f27
Shourya <shourya.bafna2019@gmail.com>
2024-12-05 17:09:01 +0530

    Made Navbar Fixed

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-items");
 
-hamburger.addEventListener("click", function (e) {
+hamburger.addEventListener("click", function () {
   navMenu.classList.toggle("dis");
   hamburger.classList.toggle("active");
 });
@@ -9,6 +9,22 @@ hamburger.addEventListener("click", function (e) {
 const skip = document.querySelector(".skip");
 const getintouch = document.querySelector(".getintouch");
 
-skip.addEventListener("click", function (e) {
+skip.addEventListener("click", function () {
   getintouch.classList.toggle("disable");
 });
+
+
+
+const navbar = document.querySelector("nav");
+
+
+window.addEventListener("scroll", function(){
+  let y = window.scrollY;
+  if (y >= 40) {
+    navbar.classList.add("scroll")
+    navbar.classList.remove("nav")
+  } else {
+    navbar.classList.add("nav")
+    navbar.classList.remove("scroll")
+  }
+})
